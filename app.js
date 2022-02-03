fd1e3a69362479146d9c57c7236a87b44bc4c5d9
Douglas <douglasonkeo3@gmail.com>
2022-02-03 22:23:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+//rendering template engine
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+// serving static files
+app.use(express.static(path.join((__dirname, 'public'))));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
