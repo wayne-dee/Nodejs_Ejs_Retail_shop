a853e7e33de82feae6dad4ed69ea15fa506e63a0
Douglas <douglasonkeo3@gmail.com>
2022-02-12 12:30:38 +0300

    refactoring the code

--- models/product.js
+++ models/product.js
@@ -1,5 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const { products } = require('../../../[Tutsgalaxy.com] - NodeJS - The Complete Guide/06 Working with Dynamic Content  Adding Templating Engines/089 05-working-on-layout-with-partials/05-working-on-layout-with-partials/routes/admin');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename), 
+    'data', 
+    'products.json'
+    );
+
+const getProductsFromFile = callback => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            callback([]);
+        }
+        else {
+            callback(JSON.parse(fileContent));
+        }
+    })
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -8,39 +26,18 @@ module.exports = class Product {
 
     save () {
         // saving product in a file
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-            );
-        
-            fs.readFile(p, (err, fileContent) => {
-                let products = [];
-                if (!err) {
-                    products = JSON.parse(fileContent)
-                }
-                products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
 
-                fs.writeFile(p, JSON.stringify(products), err => {
-                    console.log(err)
-                })
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err)
             })
+        })    
     }
 
     // static allows to fetch data directly 
     // from the Product class and not obj created
     static fetchAll (callback) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-            );
-        
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                return callback([]);
-            }
-            return callback(JSON.parse(fileContent));
-        })
+       getProductsFromFile(callback);
     }
 }
