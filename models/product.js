cf212380d531f308620e388e1a2309b7b4a2c8a1
Douglas <douglasonkeo3@gmail.com>
2022-02-13 13:48:46 +0300

    adding new views and routes in the app

--- models/product.js
+++ models/product.js
@@ -1,43 +1,40 @@
 const fs = require('fs');
 const path = require('path');
-const { products } = require('../../../[Tutsgalaxy.com] - NodeJS - The Complete Guide/06 Working with Dynamic Content  Adding Templating Engines/089 05-working-on-layout-with-partials/05-working-on-layout-with-partials/routes/admin');
 
 const p = path.join(
-    path.dirname(process.mainModule.filename), 
-    'data', 
-    'products.json'
-    );
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = callback => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            callback([]);
-        }
-        else {
-            callback(JSON.parse(fileContent));
-        }
-    })
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save () {
-        // saving product in a file
-        getProductsFromFile(products => {
-            products.push(this);
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-            fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err)
-            })
-        })    
-    }
-
-    // static allows to fetch data directly 
-    // from the Product class and not obj created
-    static fetchAll (callback) {
-       getProductsFromFile(callback);
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const productsContoller = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-
 // /admin/add-product => GET
-router.get('/add-product', productsContoller.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsContoller.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
-
