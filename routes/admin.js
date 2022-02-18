92a6c69a13475d592d62ea666172017436aacbf6
Douglas <douglasonkeo3@gmail.com>
2022-02-18 21:09:49 +0300

    editing the product data

--- routes/admin.js
+++ routes/admin.js
@@ -15,6 +15,9 @@ router.get('/products', adminController.getProducts);
 // /admin/ edit product
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
+// /admin/ post edit
+router.post('/edit-product', adminController.postEditProduct);
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
