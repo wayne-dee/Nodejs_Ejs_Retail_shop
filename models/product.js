5398393e5c8a148710ed3ad786dad01b9872ccdc
Douglas <douglasonkeo3@gmail.com>
2022-02-18 22:03:57 +0300

    deleting cart items

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart')
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -48,12 +50,15 @@ module.exports = class Product {
   }
 
   static deleteById(id) {
-    const updatedProducts = products.filter(prod => prod.id !== this.id);
-    fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-      if(!err) {
-        
-      }
-    })
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
+    });
   }
 
   static fetchAll(cb) {
