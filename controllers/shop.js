926d4b876744934197c9b02962df0c972edfa9b7
Douglas <douglasonkeo3@gmail.com>
2022-02-23 19:51:38 +0300

    retrieving products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,18 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    
 };
 
 exports.getProduct = (req, res, next) => {
