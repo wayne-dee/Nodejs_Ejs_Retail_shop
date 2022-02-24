8a999d0313f1a85ab96cfea1eabde064dc7fa869
Douglas <douglasonkeo3@gmail.com>
2022-02-24 21:25:06 +0300

    delete a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
     description: description
   }).then(result =>{
     console.log(result)
+    res.redirect('/admin/products')
   }).catch(err => {
     console.log(err)
   })
@@ -82,6 +83,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products'); 
+  Product.findByPk(prodId).then(product => {
+    return product.destroy();
+  })
+  .then(result => {
+    console.log('DESTROYED PRODUCT')
+    res.redirect('/admin/products'); 
+  })
+  .catch(err => {
+    console.log(err)
+  })
 }
