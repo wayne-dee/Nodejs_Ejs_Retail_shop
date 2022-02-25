ee13494f6fa7b00f2b3e9044a77ecb6a2994c367
Douglas <douglasonkeo3@gmail.com>
2022-02-25 10:32:25 +0300

    fetching related products

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById(1)
+  User.findByPk(1)
     .then(user => {
       req.user = user;
       next();
