9c462f3a58b52b700da63346ddd83e629812cc47
Douglas <douglasonkeo3@gmail.com>
2022-03-17 21:35:51 +0300

    getting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -126,7 +126,7 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({include: ['products']})
+  req.user.getOrders()
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
