635dd178e60cd6bc471b62ad5b11c4e9f192b5d3
Douglas <douglasonkeo3@gmail.com>
2022-03-22 21:47:45 +0300

    fetching all products

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const User = require('../models/user')
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(products => {
+  Product.find().then(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
@@ -36,15 +36,17 @@ exports.getProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll().then(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  }).catch(err => {
-    console.log(err)
-  })
+  Product.find()
+    .then(products => {
+      console.log(products)
+      res.render('shop/index', {
+          prods: products,
+          pageTitle: 'Shop',
+          path: '/'
+        });
+      }).catch(err => {
+        console.log(err)
+      })
 };
 
 exports.getCart = (req, res, next) => {
