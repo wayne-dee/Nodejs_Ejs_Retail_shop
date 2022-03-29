34814bee55200ca62ee1172ead62e00e07137599
Douglas <douglasonkeo3@gmail.com>
2022-03-29 20:51:56 +0300

    creating adding orders model and controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-// const Cart = require('../models/cart');
+const Order = require('../models/order');
 const User = require('../models/user')
 
 exports.getProducts = (req, res, next) => {
@@ -45,7 +45,7 @@ exports.getCart = (req, res, next) => {
     .populate('cart.items.productId')
     // .execPopulate()
     .then(user => {
-      products = user.cart.items;
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -111,10 +111,22 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
-    .then(result => {
+    .populate('cart.items.productId')
+    // .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: i.productId }
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      })
+      return order.save();
+    }).then(result => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
