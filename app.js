7128313782e65d109bf45039d847fe23cc0c84c0
Douglas <douglasonkeo3@gmail.com>
2022-05-04 21:16:10 +0300

    using CSRF token

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user')
@@ -12,6 +13,8 @@ const User = require('./models/user')
 const MONGODB_URI = 'mongodb+srv://onkeo:Douglous3@retailshopnode.cwxp1.mongodb.net/shop';
 
 const app = express();
+const csrfProtection = csrf();
+
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
@@ -32,6 +35,8 @@ app.use(session({
   saveUninitialized: false,
   store: store
 }));
+
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
