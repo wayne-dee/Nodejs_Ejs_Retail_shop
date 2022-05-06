c2ceace1966b189a2c89e8051e8cca150ecaa5f0
Douglas <douglasonkeo3@gmail.com>
2022-05-06 22:05:51 +0300

    providing user feedback using FLASH

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,8 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    isAuthenticated: false,
+    errorMessage: req.flash('error'),
   });
 };
 
@@ -13,7 +14,7 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    isAuthenticated: false,
   });
 };
 
@@ -52,6 +53,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({email: email})
     .then(user => {
       if (!user) {
+        req.flash('error', 'Invalid email or password')
         return res.redirect('/login')
       }
       bcrypt
