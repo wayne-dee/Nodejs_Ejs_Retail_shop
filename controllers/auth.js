97c161e86f09a245720e454e04a6073e44a903cc
Douglas <douglasonkeo3@gmail.com>
2022-06-07 19:09:45 +0300

    reset passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -141,3 +141,18 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+// RESET PASSWORD
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
