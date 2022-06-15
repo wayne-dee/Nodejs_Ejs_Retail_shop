28a2b78645f128f7ae68f8090dda8abccfe6e726
Douglas <douglasonkeo3@gmail.com>
2022-06-15 19:41:49 +0300

    handling multipart form data

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,10 +20,12 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const imageUrl = req.body.image;
   const price = req.body.price;
   const description = req.body.description;
   const errors = validationResult(req);
+
+  console.log(imageUrl)
 
   if (!errors.isEmpty()) {
     return res.status(422).render('admin/edit-product', {
