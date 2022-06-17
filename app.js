09f5086b65686ca48f9d8e96c328f64cdfe861ee
Douglas <douglasonkeo3@gmail.com>
2022-06-17 19:27:24 +0300

    Serving Images Statically

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images',express.static(path.join(__dirname, 'images'))); // serving images statically
 app.use(session({
   secret: 'my secret word', 
   resave: false, 
